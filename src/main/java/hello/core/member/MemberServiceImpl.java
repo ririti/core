d79d92f4f32e9b5c8c9f820e06077af5fc4d4805
ririti <xodnzlzl@naver.com>
2021-11-24 03:17:19 +0900

    스프링 원리 - 기본편

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {//생성자를 통해서 선택을 하게한다.
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.sava(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
